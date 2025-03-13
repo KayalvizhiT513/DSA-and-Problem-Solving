cdbc4e6a617577114f60817869a73f25ea42a9dc
Kayalvizhi T <95753452+KayalvizhiT513@users.noreply.github.com>
2025-03-14 01:29:06 +0530

    Update spinning wheel using circular linked list.py

--- spinning wheel using circular linked list.py
+++ spinning wheel using circular linked list.py
@@ -5,6 +5,7 @@ class Node:
         self.val = val
         self.prize = 0
         self.next = None
+        self.prev = None
     
 def main():
     prize_in_arc = [10000, 250, 750, 100, 1000, 500, 2500, 750, 5000, 250, 1500, 500, 2500, 100, 1500, 100]
@@ -13,25 +14,39 @@ def main():
     node = head
     for prize in prize_in_arc[1:]:
         node.next = Node(prize)
+        node.next.prev = node
         node = node.next
     node.next = head
+    head.prev = node
 
     print("Default player number is: 1")
     print("Enter q to quit and see result")
 
     player_head = Node(1)
-
-    while True:
-        
+    while True:        
         player_n = input("Enter a player number to take turn ")
         if player_n == 'q':
             break
         player_n = int(player_n)
-        print(f"Player {player_n} spinning the wheel...")
-        prize_idx = random.randint(1,1600)
+        spin_dir = input("Choose spin direction\na - anticlockwise, c - clockwise\n")
+        prize_idx = random.randint(1,50)
         node = head
-        for i in range(prize_idx):
-            node = node.next
+        if spin_dir == 'c':
+            print(f"Player {player_n} spinning the wheel...")
+            for i in range(prize_idx):
+                print(node.val, end=" : ")
+                node = node.next
+            print(node.val)
+        elif spin_dir == 'a':
+            print(f"Player {player_n} spinning the wheel...")
+            for i in range(prize_idx):
+                print(node.val, end=" : ")
+                node = node.prev
+            print(node.val)
+        else:
+            print("Oops that isn't an expected direction you lose this chance.")
+            continue
+        head = node
 
         prize = node.val
         print(f"Hurray! Player {player_n} won ${prize}")
