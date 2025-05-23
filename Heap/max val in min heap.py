c6abedea1926a3c6645f03adf95fb40adc768373
Kayalvizhi T <95753452+KayalvizhiT513@users.noreply.github.com>
2025-05-23 15:01:32 +0530

    Update max val in min heap.py

--- Heap/max val in min heap.py
+++ Heap/max val in min heap.py
@@ -1,15 +1,16 @@
 import numpy as np
+import math
 
-input_list = [18, 10, 20, 19, 17, 9, 8]
+input_list = [18, 10, 20, 19, 17, 9, 8, 73, 62, 25, 14, 33, 42, 11]
 
 def construct_min_heap(input_list, ind):
     if ind < 0 or 2*ind+2 > len(input_list):
         return input_list
     parent = input_list[ind]
     left = input_list[2*ind + 1]
-    right = input_list[2*ind + 2]
+    right = input_list[2*ind + 2] if 2*ind + 2 < len(input_list) else None
 
-    if left < right:
+    if right == None or left < right:
         if left < parent:
             input_list[ind], input_list[2*ind + 1] = left, parent
             if ind%2 == 2:
@@ -31,7 +32,22 @@ def construct_min_heap(input_list, ind):
 
 print("Min heap:", construct_min_heap(input_list, 0))
 
-n = len(input_list)
+def max_val_in_min_heap(input_list):
+    if len(input_list) == 0:
+        return None
+    n = len(input_list)
 
-lower_level = np.floor(math.log2(i))+1
+    lower_level = int(np.floor(math.log2(n))+1)
+    print("Lower level:", lower_level)
+
+    num_nodes = 2**(lower_level - 1)
+    print("Num nodes:", num_nodes)
+
+    max_val = input_list[num_nodes-1]
+    for i in range(num_nodes-1, n):
+        if input_list[i] > max_val:
+            max_val = input_list[i]
+    return max_val
+
+print("Max val in min heap:", max_val_in_min_heap(input_list))
 
